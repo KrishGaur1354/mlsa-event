0921daf570f2133799e5fca1146d406638b45a7c
ThatOneKrish <csrealracing13d@gmail.com>
2023-10-05 18:01:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, Response
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
+
+app = Flask(__name__)
+
+# Initialize the hand detector and other parameters
+detector = HandDetector(maxHands=2)
+offset = 20
+imgSize = 300
+
+cap = cv2.VideoCapture(0)
+
+
+def generate_frames():
+    while True:
+        success, img = cap.read()
+        hands, img = detector.findHands(img)
+
+        if hands:
+            hand = hands[0]
+            x, y, w, h = hand['bbox']
+            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+            imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+            imgCropShape = imgCrop.shape
+            aspectRatio = h / w
+
+            if aspectRatio > 1:
+                k = imgSize / h
+                wCal = math.ceil(k * w)
+                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                imgResizeShape = imgResize.shape
+                wGap = math.ceil((imgSize - wCal) / 2)
+                imgWhite[:, wGap:wCal + wGap] = imgResize
+
+            else:
+                k = imgSize / w
+                hCal = math.ceil(k * h)
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                imgResizeShape = imgResize.shape
+                hGap = math.ceil((imgSize - hCal) / 2)
+                imgWhite[hGap:hCal + hGap, :] = imgResize
+
+            imgWhite[0:imgCropShape[0], 0:imgCropShape[1]] = imgCrop
+
+            # Encode the processed image as JPG
+            ret, buffer = cv2.imencode('.jpg', imgWhite)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
